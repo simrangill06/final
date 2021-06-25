76ce6401fb08aa2dda35ad082abc41bbf38ef5ef
simrangill06 <86213648+simrangill06@users.noreply.github.com>
2021-06-24 23:33:15 -0400

    Add files via upload

---
+++ project/src/project1/A2project.java
@@ -0,0 +1,54 @@
+package project1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import java.util.List;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class A2project {
+	  @BeforeClass
+	    void setUpClass() {
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\localadmin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	  }
+	  @Test
+	    public void imagess() throws InterruptedException {
+	        ChromeDriver driver =new ChromeDriver();
+
+	        driver.get("https://opensource-demo.orangehrmlive.com/");
+	        List<WebElement> listImages = driver.findElements(By.tagName("img"));
+	        int figures = 0;
+	        for (WebElement image : listImages){
+	            if ( image.isDisplayed()){
+	                figures++;
+	            }
+	        }
+	        System.out.println("No. of total displayed images: " + figures);
+	        driver.close();
+	  }
+	  @Test
+	    public void buttons() throws InterruptedException {
+	        ChromeDriver driver =new ChromeDriver();
+
+	        driver.get("https://opensource-demo.orangehrmlive.com/");
+	        List<WebElement> listButtons = driver.findElements(By.tagName("button"));
+
+	        int counts = 0;
+	        for (WebElement button : listButtons) {
+	            if(button.getText().equals("buttonName")) {
+	                counts++;
+	            }
+	        }
+	        System.out.println("No. of total displyed buttons: " + counts);
+
+	        driver.close();
+	    }
+
+
+
+	}
---
+++ project/src/project1/Aproject.java
@@ -0,0 +1,20 @@
+package project1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test; class Automated {
+
+    @Test
+    public void simmy(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\localadmin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        String actualTitle = "Google";
+        Assert.assertEquals(driver.getTitle(), actualTitle );
+       WebElement logo = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/img")  );
+      Assert.assertEquals(true, logo.isDisplayed());
+        System.out.println("pass");
+        driver.close();
+}
+}
